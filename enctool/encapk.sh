0486727a5121dc630f2083f22df8d33df7c182b5
cw-jeonhoeun <jeonhoeun@coway.co.kr>
2021-05-03 12:52:45 +0900

    edit encapk.sh

--- enctool/encapk.sh
+++ enctool/encapk.sh
@@ -1,4 +1,4 @@
 
-java -jar JavaRSA.jar ../app/build/outputs/apk/release/app-release-unsigned-signed.apk ../release.enc
+java -jar enctool/JavaRSA.jar app/build/outputs/apk/release/app-release-unsigned-signed.apk release.enc
 
 
